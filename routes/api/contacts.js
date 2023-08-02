84b47b30ce1d6afd1a88916f96cbb588ff8aa4b0
Pavel_soundNotLight <105560877+PavTrap@users.noreply.github.com>
2023-08-02 23:32:49 +0300

    hw4

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,17 +1,17 @@
 const express = require("express");
 
 const ctrl = require("../../controllers/contacts");
-const { validateBody, isValidId, isValidFavorite } = require("../../middlewares");
+const { validateBody, isValidId, isValidFavorite, authenticate } = require("../../middlewares");
 const {schemas} = require("../../models/contact");
 
 const router = express.Router();
 
-router.get("/", ctrl.listContacts);
-router.get("/:contactId", isValidId, ctrl.getContactById);
-router.post("/", validateBody(schemas.addSchema), ctrl.addContact);
-router.put("/:contactId", isValidId, validateBody(schemas.addSchema), ctrl.updateContactById);
-router.patch("/:contactId/favorite", isValidId, isValidFavorite(schemas.updateFavoriteSchema), ctrl.updateFavorite)
-router.delete("/:contactId", isValidId, ctrl.removeContact);
+router.get("/", authenticate, ctrl.listContacts);
+router.get("/:contactId", authenticate, isValidId, ctrl.getContactById);
+router.post("/", authenticate, validateBody(schemas.addSchema), ctrl.addContact);
+router.put("/:contactId", authenticate, isValidId, validateBody(schemas.addSchema), ctrl.updateContactById);
+router.patch("/:contactId/favorite", authenticate, isValidId, isValidFavorite(schemas.updateFavoriteSchema), ctrl.updateFavorite)
+router.delete("/:contactId", authenticate, isValidId, ctrl.removeContact);
 
 module.exports = router;
 
